bf77379dfd2040a2057eedb6cd4de6efbf49c684
Anagha M Anil Kumar <43451020+Anagha18@users.noreply.github.com>
2021-12-12 19:10:54 -0800

    Inputs API Exposed

--- InputExposed_Bplustree/write_file.cpp
+++ InputExposed_Bplustree/write_file.cpp
@@ -1,21 +1,65 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include "cxxopts.h"
+
+# define DEFAULT_RANGE "1000000"
+#define DEFAULT_TYPE "1"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    int iSecret;
-    srand(time(NULL));
+    cxxopts::Options options("pi_calculation",
+                           "Calculate pi using serial and parallel execution");
+  options.add_options(
+      "custom",
+      {
+          {"nRange", "N",
+           cxxopts::value<uint>()->default_value(DEFAULT_RANGE)},
+          {"type", "Type of Input to Generate",
+           cxxopts::value<uint>()->default_value(DEFAULT_TYPE)},
+      });
 
-    ofstream myfile;
-    myfile.open("written.txt");
-    int n = 1000000;
+  auto cl_options = options.parse(argc, argv);
+  uint range_max= cl_options["nRange"].as<uint>();
+  uint input_typ = cl_options["type"].as<uint>();
+  
+  std::cout << std::fixed;
+  std::cout << "Insert from 1 to "<<range_max <<"\n";
+  std::cout << "Option Chosen : ";
+  if(input_typ==1)
+  {
+      std::cout << "Serial Input.\n";
+  }
+  else if(input_typ==2)
+  {
+      std::cout << "Random Input.\n";
+  }
+  int n= range_max;
+  int iSecret;
+  srand(time(NULL));
+  ofstream myfile;
+ myfile.open("written.txt");
+switch(input_typ)
+{
+case 1:
+        for(int i=1;i<=n;i++)
+        {
+            myfile << i << " ";
+        }
+break;
+    case 2:
+
+    // int n = 1000000;
     for (int i = 1; i <= n; i++)
     {
         iSecret = rand() % n + 1;
         myfile << iSecret << " ";
     }
-    myfile.close();
+    
+    break;
+}
+myfile.close();
+std::cout << "Done \n";
     return 0;
 }
