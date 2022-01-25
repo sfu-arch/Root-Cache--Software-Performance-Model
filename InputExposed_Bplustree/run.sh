c0ab8a06e6fe18f8066b1e46bd5e66683ad9e9c8
root <adityaabhiram3@gmail.com>
2022-01-25 17:18:29 +0000

    More Analysis

--- InputExposed_Bplustree/run.sh
+++ InputExposed_Bplustree/run.sh
@@ -1,5 +1,5 @@
 g++ search_file.cpp -o search
-./search --nRange 10000000 --type 4 --srange 1000000  --erange 9000000 --randomness 100
+./search --nRange 10000000 --type 4 --srange 3000000  --erange 7000000 --randomness 100
 g++ write_file.cpp -o write 
 ./write --nRange 10000000 --type 1
 
